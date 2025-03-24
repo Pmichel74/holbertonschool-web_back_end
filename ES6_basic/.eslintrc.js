4577719e7e145ffabfc3dc1ef00de3452ccf5f38
Pmichel74 <patrick.yann.michel@gmail.com>
2025-03-24 19:14:01 +0100

    task7

--- ES6_basic/.eslintrc.js
+++ ES6_basic/.eslintrc.js
@@ -12,4 +12,39 @@ module.exports = {
   "rules": {
     "indent": ["error", 2]
   }
+};.js
+module.exports = {
+  env: {
+    browser: true,
+    es6: true,
+    jest: true,
+  },
+  extends: [
+    'airbnb-base',
+    'plugin:jest/all',
+  ],
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly',
+  },
+  parserOptions: {
+    ecmaVersion: 2018,
+    sourceType: 'module',
+  },
+  plugins: ['jest'],
+  rules: {
+    'no-console': 'off',
+    'no-shadow': 'off',
+    'no-restricted-syntax': [
+      'error',
+      'LabeledStatement',
+      'WithStatement',
+    ],
+  },
+  overrides:[
+    {
+      files: ['*.js'],
+      excludedFiles: 'babel.config.js',
+    }
+  ]
 };
