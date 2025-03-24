eeb47d33eae3601b0a3bb0e71711906cd9db6122
Pmichel74 <patrick.yann.michel@gmail.com>
2025-03-24 17:09:59 +0100

    task2

--- ES6_basic/1-main.js
+++ ES6_basic/1-main.js
@@ -1,4 +1,11 @@
-import taskBlock from './1-block-scoped.js';
+import taskBlock from './1-block-scoped';
 
-console.log(taskBlock(true));
-console.log(taskBlock(false));
+describe('Block scoped variables testing', () => {
+  test('taskBlock(true) should return [false, true]', () => {
+    expect(taskBlock(true)).toEqual([false, true]);
+  });
+
+  test('taskBlock(false) should return [false, true]', () => {
+    expect(taskBlock(false)).toEqual([false, true]);
+  });
+});
