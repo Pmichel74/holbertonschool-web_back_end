763504b90befbed9a085c3c5a467a23152ea4d9a
Pmichel74 <patrick.yann.michel@gmail.com>
2025-03-24 18:16:25 +0100

    task3-6

--- ES6_basic/3-default-parameter.js
+++ ES6_basic/3-default-parameter.js
@@ -1,10 +1,4 @@
-/**
- * Calculate sum of initialNumber and expansion values
- * @param {number} initialNumber - The initial number
- * @param {number} [expansion1989=89] - The 1989 expansion value
- * @param {number} [expansion2019=19] - The 2019 expansion value
- * @return {number} The sum of all three parameters
- */
+// Execute: npm run dev 3-default-parameter.js
 export default function getSumOfHoods(initialNumber, expansion1989 = 89, expansion2019 = 19) {
   return initialNumber + expansion1989 + expansion2019;
 }
