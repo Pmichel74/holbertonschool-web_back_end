4c5a8dfc3cda74d68fa82c7d8b48a6ad835c49ad
Pmichel74 <patrick.yann.michel@gmail.com>
2025-03-25 18:40:23 +0100

    task 0-2

--- ES6_basic/12-createReportObject.js
+++ ES6_basic/12-createReportObject.js
@@ -1,7 +1,7 @@
 export default function createReportObject(employeesList) {
   return {
     allEmployees: { ...employeesList },
-    getNumberOfDepartments(employeesList) {
+    getNumberfDepartments(employeesList) {
       return Object.keys(employeesList).length;
     },
   };
