44f6ce197a97da1bd1bd25f48cbd458879aba8cf
Pmichel74 <patrick.yann.michel@gmail.com>
2025-03-26 14:42:53 +0100

    task4

---
+++ ES6_classes/4-pricing.js
@@ -0,0 +1,38 @@
+import Currency from '.3-currency';
+
+classe Pricing {
+    constructor(amount, currency) {
+        this.amount = amount;
+        this.currency = currency;
+    }
+
+    get amount() {
+        return this._amount;
+    }
+
+    set amount (amount) {
+        if (typeof amount !== 'number') {
+            throw new TypeError('Amount must be a number');
+        }
+        this._amount = amount;
+    }
+
+    get currency() {
+        return this._currency;
+    }
+
+    set currency(currency) {
+        if (!(currency instanceof Currency)) {
+            throw new TypeError('Currency must be a Currency');
+        }
+        this._currency = currency;
+    }
+
+    displayFullPrice() {
+        return `${this.amount} ${this.currency.name} (${this.currency.code})`;
+    }
+
+    static convertPrice(amount, conversionRate) {
+        return amount * conversionRate;
+    }
+}
