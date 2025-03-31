1bd4cd8c95cbd7ff0dc6da5d0e558f7c2f2be29a
Pmichel74 <patrick.yann.michel@gmail.com>
2025-03-31 19:19:14 +0200

    task4

--- ES6_data_manipulation/4-update_grade_by_city.js
+++ ES6_data_manipulation/4-update_grade_by_city.js
@@ -1,11 +1,8 @@
-export default function updateStudentGradeByCity(students, city, newGrades) {
-  return students
+export default function updateStudentGradeByCity(arr, city, newGrades) {
+  return arr
     .filter((student) => student.location === city)
     .map((student) => {
-      const studentGrade = newGrades.find((grade) => grade.studentId === student.id);
-      return {
-        ...student,
-        grade: (grade && grade.grade) || 'N/A',
-      };
+      const grade = newGrades.find((g) => g.studentId === student.id);
+      return { ...student, grade: (grade && grade.grade) || 'N/A' };
     });
 }
