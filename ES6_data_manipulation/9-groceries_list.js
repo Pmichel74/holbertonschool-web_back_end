d8021db8375382870e00d17e70b92f895d0b862b
Pmichel74 <patrick.yann.michel@gmail.com>
2025-04-02 19:29:47 +0200

    task9

---
+++ ES6_data_manipulation/9-groceries_list.js
@@ -0,0 +1,10 @@
+export default function groceriesList() {
+  const map = new Map();
+  map.set(1, 'Apples');
+  map.set(2, 'Tomatoes');
+  map.set(3, 'Pasta');
+  map.set(4, 'Rice');
+  map.set(5, 'Banana');
+
+  return map;
+}
