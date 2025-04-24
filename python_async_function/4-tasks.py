d9fd62d76525e4b3fc4d5295ab4b6783d2005f45
Pmichel74 <patrick.yann.michel@gmail.com>
2025-04-24 15:09:21 +0200

    task 1-3

--- python_async_function/4-tasks.py
+++ python_async_function/4-tasks.py
@@ -1,34 +1,14 @@
 #!/usr/bin/env python3
-"""Module contains function that takes two integers
-
-Imports:
-    typing: module for list type annotation
-    asyncio: module for concurrency
-    task_wait_random: function that returns an asyncio.Task
-"""
+''' async and await syntax '''
 import asyncio
 from typing import List
-# This import assumes '3-tasks' module provides 'task_wait_random'
-# even if the file content shown earlier seemed different.
-task_wait_random = __import__('3-tasks').task_wait_random
+
+get = __import__('3-tasks').task_wait_random
 
 
 async def task_wait_n(n: int, max_delay: int) -> List[float]:
-    """Function takes integers and calls task_wait_random function concurrently
-
-    Args:
-        n (int): num of times to call task_wait_random
-        max_delay (int): Num of seconds to delay task_wait_random
-
-    Returns:
-        List[float]: List of task_wait_random results in ascending order
-    """
-    tasks = [asyncio.create_task(task_wait_random(max_delay)) for _ in range(n)]
-    delays = await asyncio.gather(*tasks)
-
-    # Manual bubble sort (as was present in the original 4-tasks.py)
-    for end in range(len(delays), 1, -1):
-        for j in range(1, end):
-            if delays[j - 1] > delays[j]:
-                delays[j - 1], delays[j] = delays[j], delays[j - 1]
-    return delays
+    ''' Function that returns a list
+    '''
+    s = [get(max_delay) for i in range(n)]
+    finish = [await task for task in asyncio.as_completed(l)]
+    return finish
