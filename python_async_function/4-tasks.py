d64ea730ca171d00ab545920f176cfd46de7a215
Pmichel74 <patrick.yann.michel@gmail.com>
2025-04-24 14:37:42 +0200

    task 1-3

--- python_async_function/4-tasks.py
+++ python_async_function/4-tasks.py
@@ -1,34 +1,27 @@
 #!/usr/bin/env python3
-"""Module contains function that takes two integers
-
-Imports:
-    typing: module for list type annotation
-    asyncio: module for concurrency
-    task_wait_random: function that returns an asyncio.Task
-"""
+"""Multiple coroutines"""
 import asyncio
 from typing import List
-# This import assumes '3-tasks' module provides 'task_wait_random'
-# even if the file content shown earlier seemed different.
-task_wait_random = __import__('3-tasks').task_wait_random
 
 
 async def task_wait_n(n: int, max_delay: int) -> List[float]:
-    """Function takes integers and calls task_wait_random function concurrently
-
-    Args:
-        n (int): num of times to call task_wait_random
-        max_delay (int): Num of seconds to delay task_wait_random
-
-    Returns:
-        List[float]: List of task_wait_random results in ascending order
     """
-    tasks = [asyncio.create_task(task_wait_random(max_delay)) for _ in range(n)]
-    delays = await asyncio.gather(*tasks)
+    --------------
+    METHOD: wait_n
+    --------------
+    Description:
+        Executes multiple coroutines at
+        the same time with async
+    Args:
+        @n: runs wait_random n number of times
+        @max_delay: specifies the max delay with
+        each run of wait_random
+    """
+    task_wait_random = __import__('3-tasks').task_wait_random
+    delay_list = [task_wait_random(max_delay) for i in range(n)]
+    completed_tasks = []
 
-    # Manual bubble sort (as was present in the original 4-tasks.py)
-    for end in range(len(delays), 1, -1):
-        for j in range(1, end):
-            if delays[j - 1] > delays[j]:
-                delays[j - 1], delays[j] = delays[j], delays[j - 1]
-    return delays
+    for task in asyncio.as_completed(delay_list):
+        completed_tasks.append(await task)
+
+    return completed_tasks
