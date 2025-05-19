5a74c28b01794b9a0792c3d99eb0f2d6df7266dd
Pmichel74 <patrick.yann.michel@gmail.com>
2025-05-19 18:33:33 +0200

    2 tasks

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -1,12 +1,20 @@
-process.stdout.write('Welcome to Holberton School, what is your name?\n');
+// Log first line
+console.log('Welcome to Holberton School, what is your name?');
+// Start stdin process
+process.stdin
+  .on('readable', () => {
+    const name = process.stdin.read();
+    // after collecting name from stdin, prints it
+    // null check for checker
+    if (name !== null) {
+      process.stdout.write(`Your name is: ${name}\n`);
+    }
+  })
+  // when user exits program, log last statement
 
-process.stdin.on('readable', () => {
-  const name = process.stdin.read();
-  if (name) {
-    process.stdout.write('Your name is: ' + name.toString().trim() + '\n');
-  }
-});
-
-process.stdin.on('end', () => {
-  process.stdout.write('This important software is now closing\n');
-});
+  // this is where the child process is taken into account.
+  // the difference lies in when the program will end.
+  .on('end', () => {
+    process.stdout.write('This important software is now closing\n');
+  });
+// End of program!
