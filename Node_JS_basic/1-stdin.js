fbe2fb4be4143fd0036e722b4a7981c012b5e3b1
Pmichel74 <patrick.yann.michel@gmail.com>
2025-05-19 16:54:05 +0200

    2 tasks

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -4,9 +4,7 @@ std.stdout.write('Welcome to Holberton School, what is your name?\n');
 std.stdin.on('readable', () => {
   const name = std.stdin.read();
   if (name) {
-    // Convertir name en string et supprimer tout saut de ligne à la fin
-    const nameStr = name.toString().trim();
-    std.stdout.write(`Your name is: ${nameStr}\n`);
+    std.stdout.write(`Your name is: ${name}`);
   }
 });
 std.stdin.on('end', () => {
