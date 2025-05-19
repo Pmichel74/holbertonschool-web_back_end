8665f6880a3a0960e4b2ec39872799771c44f72e
Pmichel74 <patrick.yann.michel@gmail.com>
2025-05-19 16:38:56 +0200

    2 tasks

--- Node_JS_basic/2-es6-main.js
+++
@@ -1,9 +0,0 @@
-// Test file for ES6 code
-const countStudents = require('./2-es6');
-
-// Sample data
-const studentList = ['Alice', 'Bob', 'Charlie', 'David'];
-
-// Test the function
-const report = countStudents(studentList);
-console.log('Report object:', report);
