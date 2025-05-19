fa7f2dff9fe415f878f9fcffd47227a725d42d30
Pmichel74 <patrick.yann.michel@gmail.com>
2025-05-19 18:31:04 +0200

    2 tasks

--- Node_JS_basic/0-console.js
+++ Node_JS_basic/0-console.js
@@ -1,9 +1,5 @@
-/**
- * Function that displays a message on the console
- * @param {string} message - The message to display
- */
-function displayMessage(message) {
-  console.log(message);
+function displayMessage(str) {
+  console.log(str);
 }
 
 module.exports = displayMessage;
