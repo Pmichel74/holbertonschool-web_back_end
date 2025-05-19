3b00c663844106f1088db1a853619c903e3dc356
Pmichel74 <patrick.yann.michel@gmail.com>
2025-05-19 16:47:19 +0200

    2 tasks

--- Node_JS_basic/0-console.js
+++ Node_JS_basic/0-console.js
@@ -1,3 +1,11 @@
+/**
+ * Displays a message in the standard output (STDOUT)
+ * This function takes a string argument and prints it to the console
+ * using Node.js console.log method, which writes to process.stdout
+ *
+ * @param {string} message - The message to be displayed in the console
+ * @returns {void} This function does not return a value
+ */
 function displayMessage(message) {
   console.log(message);
 }
