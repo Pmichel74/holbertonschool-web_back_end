73876442117c059070603d3f1ba82f040e6dda71
Pmichel74 <patrick.yann.michel@gmail.com>
2025-05-23 15:44:48 +0200

    task6

---
+++ Node_JS_basic/6-http_express.js
@@ -0,0 +1,19 @@
+// Import the express module to create an HTTP server
+const myExpress = require('express');
+
+// Create an Express application instance
+const app = myExpress();
+
+// Start the server and listen on port 1245
+app.listen(1245, () => {
+  // Log a message when the server is ready
+  console.log('API available on localhost port 1245');
+});
+
+// Define a route for the root URL that sends a welcome message
+app.get('/', (req, res) => {
+  res.send('Hello Holberton School!');
+});
+
+// Export the app for use in other modules (e.g., for testing)
+module.exports = app;
