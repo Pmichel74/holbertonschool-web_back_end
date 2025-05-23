d4c052468a3ffedfc1cf2ea2e4b66b1c4ca33715
Pmichel74 <patrick.yann.michel@gmail.com>
2025-05-23 10:27:00 +0200

    create server

--- Node_JS_basic/test/1-stdin.test.js
+++
@@ -1,9 +0,0 @@
-const { expect } = require('chai');
-const sinon = require('sinon');
-
-describe('stdin script', () => {
-  it('script exists', () => {
-    const fs = require('fs');
-    expect(fs.existsSync('./1-stdin.js')).to.be.true;
-  });
-});
