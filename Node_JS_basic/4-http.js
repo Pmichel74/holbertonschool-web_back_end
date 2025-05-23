a203f883e9d01812e9b84fb4218c3bfef8852e16
Pmichel74 <patrick.yann.michel@gmail.com>
2025-05-23 18:28:07 +0200

    4

--- Node_JS_basic/4-http.js
+++ Node_JS_basic/4-http.js
@@ -1,18 +1,10 @@
-// Import the http module to create an HTTP server
 const http = require('http');
 
-// Create the HTTP server
 const app = http.createServer((req, res) => {
-  // Set the response header to indicate plain text content and status 200 (OK)
   res.writeHead(200, { 'Content-Type': 'text/plain' });
-  // Write the response body
-  res.write('Hello Holberton School!');
-  // End the response
-  res.end();
+  res.end('Hello Holberton School!');
 });
 
-// Start the server and listen on port 1245
 app.listen(1245);
 
-// Export the app for use in other modules (e.g., for testing)
 module.exports = app;
